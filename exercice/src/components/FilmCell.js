18ca0af9c2e3f734527d76bf4a1e1e9905a069c0
Cyril Pierron <cype59@gmail.com>
2020-04-16 15:29:17 +0200

    Ajout compteur et bar de like/dislike

--- exercice/src/components/FilmCell.js
+++ exercice/src/components/FilmCell.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, CardHeader, CardBody, Col, Row } from 'reactstrap';
+import { Card, CardHeader, CardBody, Col, Row, Button } from 'reactstrap';
 import '../style/FilmCell.css';
 
 class FilmCell extends React.Component {
@@ -8,16 +8,19 @@ class FilmCell extends React.Component {
   }
   render() {
     return (
-      <Col xs="4">
+      <Col lg="3" md="4" xs="12" sm="6">
         <Card className="carte">
-          <CardHeader>{this.props.movies.title}</CardHeader>
+          <CardHeader className="titre">{this.props.movies.title}</CardHeader>
           <CardBody>
-            <Row>
-              <Col><img src={this.props.movies.img} alt="affiche" className="affiche" /></Col>
-              <Col className="infos">
-                <p>Catégorie : {this.props.movies.category}</p>
-              </Col>
-            </Row>
+            <Row><img src={this.props.movies.img} alt="affiche" className="affiche" /></Row>
+            <p className="categorie">Catégorie : {this.props.movies.category}</p>
+            <Button className="like" onClick={() => this.props.compteLikes(this.props.movies.id)}><i className="fas fa-thumbs-up"></i></Button>
+            {this.props.movies.likes}
+            <Button className="like" onClick={() => this.props.compteDislikes(this.props.movies.id)}><i class="fas fa-thumbs-down"></i></Button>
+            {this.props.movies.dislikes}
+            <div className="bar">
+              <progress id="avancement" className="progressBar" value={(this.props.movies.likes * 100 / (this.props.movies.likes + this.props.movies.dislikes)).toFixed(2)} max="100"></progress>
+            </div>
           </CardBody>
         </Card>
       </Col>
--- exercice/src/components/FilmList.js
+++ exercice/src/components/FilmList.js
@@ -4,24 +4,49 @@ import FilmCell from './FilmCell.js';
 import { Container, Row } from 'reactstrap'
 
 
+class FilmList extends React.Component {
+    constructor(props) {
+        super(props)
+        this.handleCompteLikes = this.handleCompteLikes.bind(this)
+        this.handleCompteDislikes = this.handleCompteDislikes.bind(this)
+        this.state = {
+            films: movies
+        }
+    }
 
-const FilmList = () => {
-    const cells = movies.map(movies => {
+    handleCompteLikes = (id_select) => {
+        const compteur = this.state.films[id_select-1].likes + 1
+        const infos = this.state.films
+        infos[id_select-1].likes = compteur
+        this.setState(infos)
+    }
+
+    handleCompteDislikes = (id_select) => {
+        const compteur = this.state.films[id_select-1].dislikes + 1
+        const infos = this.state.films
+        infos[id_select-1].dislikes = compteur
+        this.setState(infos)
+    }
+
+    render() {
+        const cells = this.state.films.map(movies => {
+            return (
+                <FilmCell
+                    key={movies.id}
+                    movies={movies}
+                    compteLikes={this.handleCompteLikes}
+                    compteDislikes={this.handleCompteDislikes}
+                />
+            );
+        });
         return (
-            <FilmCell
-                key={movies.id}
-                movies={movies}
-            />
-        );
-    });
-
-    return (
-        <Container>
-            <Row>
-                {cells}
-            </Row>
-        </Container>
-    )
+            <Container>
+                <Row>
+                    {cells}
+                </Row>
+            </Container>
+        )
+    }
 }
 
 export default FilmList;
